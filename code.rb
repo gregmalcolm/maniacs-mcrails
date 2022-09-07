48110f7ee52a10ecf771381df77607570642aa5c
Greg Malcolm <greg.malcolm@beam.dental>
2022-09-06 21:22:05 -0400

    +to_s

--- code.rb
+++ code.rb
@@ -1,10 +1,11 @@
 module McRails
   class EnvProxy
+    def to_s
+      ENV['RAILS_ENV'] || 'development'
+    end
   end
 
   def self.env
-    #ENV['RAILS_ENV'] || 'development'
-
     proxy = EnvProxy.new
     proxy
   end
